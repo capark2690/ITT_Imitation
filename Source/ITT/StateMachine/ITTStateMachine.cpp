9f0486e56b2f3f4bbbc8644775f3624e43829413
capark2690 <capark2690@gmail.com>
2024-10-07 23:53:09 +0900

    Add - [System] StateMachine 추가

---
+++ Source/ITT/StateMachine/ITTStateMachine.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ITTStateMachine.h"
+
+
+UITTStateMachine::UITTStateMachine()
+	: PreviousStateId(-1), CurrentStateId(-1), NextStateId(-1)
+{
+}
+
+
+void UITTStateMachine::BuiltInInitialize(const FName& _StateMachineName)
+{
+	StateMachineName = _StateMachineName;
+}
+
+
+// ========== Change ========== //
+void UITTStateMachine::SetNextState(uint32 Id, bool bChangeImmediately)
+{
+	NextStateId = Id;
+
+	if (bChangeImmediately)
+	{
+		ChangeToNextState();
+	}
+}
+
+int64 UITTStateMachine::ChangeToNextState()
+{
+	if (NextStateId >= 0)
+	{
+		SetState_Internal(NextStateId);
+	}
+
+	return NextStateId;
+}
+
+void UITTStateMachine::ResetState()
+{
+	PreviousStateId = -1;
+	CurrentStateId = -1;
+	NextStateId = -1;
+}
+
+void UITTStateMachine::SetState_Internal(uint32 Id)
+{
+	if (Id != CurrentStateId)
+	{
+		PreviousStateId = CurrentStateId;
+		CurrentStateId = Id;
+	}
+	
+	bool bDelegateBound = OnChangeStateDelegate.ExecuteIfBound(PreviousStateId, CurrentStateId);
+	if (!bDelegateBound)
+	{
+		ITTLOG(Log, TEXT("[%s] OnChangeStateDelegate isn't bound [StateMachineName : %s]")
+			, *ITTSTRING_FUNC, *StateMachineName.ToString());
+	}
+}
+// ============================ //
+
+
+// ==================== Getter ==================== //
+// ========== State ========== //
+bool UITTStateMachine::IsStateDirty() const
+{
+	return NextStateId >= 0 && CurrentStateId != NextStateId;
+}
+// =========================== //
+// ================================================ //
---
+++ Source/ITT/StateMachine/ITTStateMachine.h
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ITTStateMachine.generated.h"
+
+/**
+ * 
+ */
+DECLARE_DELEGATE_TwoParams(FITTOnChangeStateDelegate, int64, int64);
+
+
+UCLASS()
+class ITT_API UITTStateMachine : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UITTStateMachine();
+
+	
+	void BuiltInInitialize(const FName& _StateMachineName);
+
+	
+	// ========== Change ========== //
+	void SetNextState(uint32 Id, bool bChangeImmediately = true);
+	int64 ChangeToNextState();
+	
+	void ResetState();
+	
+private:
+	void SetState_Internal(uint32 Id);
+	// ============================ //
+	
+	
+public:
+	// ==================== Getter ==================== //
+	// ========== State Machine ========== //
+	const FName& GetStateMachineName() const { return StateMachineName; }
+	// =================================== //
+
+
+	// ========== State ========== //
+	int64 GetPreviousStateId() const { return PreviousStateId; }
+	
+	int64 GetCurrentStateId() const { return CurrentStateId; }
+	
+	int64 GetNextStateId() const { return NextStateId; }
+
+	bool IsStateDirty() const;
+	// =========================== //
+	// ================================================ //
+	
+	
+private:
+	// ========== State Machine ========== //
+	FName StateMachineName;
+	// =================================== //
+
+	
+	// ========== State ========== //
+	int64 PreviousStateId;
+	int64 CurrentStateId;
+	int64 NextStateId;
+	// =========================== //
+	
+	
+public:
+	// ==================== Delegate ==================== //
+	// ========== Change ========== //
+	FITTOnChangeStateDelegate OnChangeStateDelegate;
+	// ============================ //
+	// ================================================== //
+};
